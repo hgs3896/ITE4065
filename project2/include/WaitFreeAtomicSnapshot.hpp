137a69ea2176cbd68db99dc84cb61ac89587f349
Ji Seong Ham <hgs3896@naver.com>
2022-10-11 00:44:57 +0900

    Unique_ptr version

--- project2/include/WaitFreeAtomicSnapshot.hpp
+++ project2/include/WaitFreeAtomicSnapshot.hpp
@@ -11,15 +11,7 @@ template<typename T>
 struct WaitFreeAtomicSnapshotItem{
     T data;
     uint64_t version;
-    std::shared_ptr<Snapshot<T>> last;
-
-    WaitFreeAtomicSnapshotItem() = default;
-    WaitFreeAtomicSnapshotItem(T data, uint64_t version, std::shared_ptr<Snapshot<T>> last): data(data), version(version), last(last) {}
-    WaitFreeAtomicSnapshotItem(const WaitFreeAtomicSnapshotItem& other) = default;
-    WaitFreeAtomicSnapshotItem(WaitFreeAtomicSnapshotItem&& other) = default;
-    WaitFreeAtomicSnapshotItem& operator=(const WaitFreeAtomicSnapshotItem& other) = default;
-    WaitFreeAtomicSnapshotItem& operator=(WaitFreeAtomicSnapshotItem&& other) = default;
-    ~WaitFreeAtomicSnapshotItem() = default;
+    std::unique_ptr<Snapshot<T>> last;
 };
 
 template<typename T>
@@ -33,8 +25,8 @@ private:
     std::vector<std::shared_ptr<WaitFreeAtomicSnapshotItem<T>>> items;
     std::vector<size_t> num_updates;
 
-    std::shared_ptr<Snapshot<T>> collect() const{
-        std::shared_ptr<Snapshot<T>> snap = std::make_shared<Snapshot<T>>();
+    std::unique_ptr<Snapshot<T>> collect() const{
+        std::unique_ptr<Snapshot<T>> snap = std::make_unique<Snapshot<T>>();
         snap->items.resize(this->items.size());
         
         for(size_t i = 0; i < this->items.size(); i++){
@@ -50,7 +42,6 @@ public:
         for (size_t i = 0; i < n; i++)
         {
             items[i] = std::make_shared<WaitFreeAtomicSnapshotItem<T>>();
-            // items[i] = new WaitFreeAtomicSnapshotItem<T>();
         }
     }
 
@@ -58,7 +49,7 @@ public:
         // Defered destruction
         for(size_t i = 0;i < items.size();i++){
             while(items[i]->last != nullptr){
-                std::shared_ptr<Snapshot<T>> to_be_killed;
+                std::unique_ptr<Snapshot<T>> to_be_killed;
                 std::swap(to_be_killed, items[i]->last);
                 items[i]->last = std::move(to_be_killed->items[i]->last);
             }
@@ -68,11 +59,14 @@ public:
     void update(size_t tid, T new_data){
         // Take a scan
         auto collect = scan();
-        
+        assert(collect);
+
         // Build a new Wait-Free Atomic Snapshot Item
-        auto item = std::make_shared<WaitFreeAtomicSnapshotItem<T>>(
-            new_data, collect->items[tid]->version + 1, collect
-        );
+        auto item = std::make_shared<WaitFreeAtomicSnapshotItem<T>>();
+        item->data = new_data;
+        item->version = collect->items[tid]->version + 1;
+        item->last = std::move(collect);
+        assert(item->last);
         
         // Atomic Update the WaitFreeAtomicSnapshotItem
         std::atomic_store(&this->items[tid], std::move(item));
@@ -81,21 +75,23 @@ public:
         ++num_updates[tid];
     }
 
-    std::shared_ptr<Snapshot<T>> scan() const {
+    std::unique_ptr<Snapshot<T>> scan() const {
         const size_t item_size = items.size();
         std::vector<bool> moved(item_size);
         size_t i;
 
         using CollectType = decltype(collect());
         CollectType old_collect = collect();
+        assert(old_collect);
         CollectType new_collect;
 
         while(true){
             new_collect = collect();
+            assert(new_collect);
             for(i = 0;i < item_size;i++){
                 if(old_collect->items[i]->version != new_collect->items[i]->version){
                     if(moved[i]) {
-                        return new_collect->items[i]->last;
+                        return std::make_unique<Snapshot<T>>(*new_collect->items[i]->last);
                     }else{
                         moved[i] = true;
                         break;
@@ -108,6 +104,7 @@ public:
 
             old_collect = std::move(new_collect);
         }
+        assert(new_collect);
         return new_collect;
     }
 
@@ -117,7 +114,7 @@ public:
 };
 
 template<typename T>
-inline std::ostream& operator<<(std::ostream& os, const std::shared_ptr<Snapshot<T>>& snapshot) {
+inline std::ostream& operator<<(std::ostream& os, const std::unique_ptr<Snapshot<T>>& snapshot) {
     size_t n = snapshot->items.size();
     
     os << "Snapshot(" << snapshot->items[0]->data;
